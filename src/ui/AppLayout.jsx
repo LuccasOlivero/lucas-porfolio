2b7a2198f3cc2d81d4ccbf92f4b886b8eef7cd15
lucas <lucas.chorolqui@gmail.com>
2023-11-28 23:51:08 -0300

    updates

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -53,7 +53,25 @@ function AppLayout() {
         <Circle type="bigger" />
         {/* <Circle type="bigger2" /> */}
 
-        <Section></Section>
+        <Section>
+          <Row type="borderTop">
+            <Span>
+              <Logo />
+              Selected Work & projects
+            </Span>
+          </Row>
+
+          <Work />
+
+          <Row type="bigger2">
+            <Circle position="bottom">
+              find out <br /> more
+              <Span type="mleft">
+                <FontAwesomeIcon icon={faArrowDownLong} />
+              </Span>
+            </Circle>
+          </Row>
+        </Section>
 
         <TextCarousel text2="skils" type="skils" />
 
